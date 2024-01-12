e42c4a9e4a1133c68b5d078c6969a15a32651eca
zceelsc <76179368+zceelsc@users.noreply.github.com>
2024-01-12 19:20:05 +0000

    Update A.py

--- A.py
+++ A.py
@@ -15,35 +15,12 @@ received_var=sys.argv[1]
 class Net(nn.Module):
     def __init__(self, in_channels, num_classes):
         super(Net, self).__init__()
-
-        """self.layer1 = nn.Sequential(nn.Conv2d(in_channels, 16, kernel_size=3           ), nn.BatchNorm2d(16), nn.ReLU())
-        self.layer2 = nn.Sequential(nn.Conv2d(16,          16, kernel_size=3           ), nn.BatchNorm2d(16), nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer3 = nn.Sequential(nn.Conv2d(16,          64, kernel_size=3           ), nn.BatchNorm2d(64), nn.ReLU())
-        self.layer4 = nn.Sequential(nn.Conv2d(64,          64, kernel_size=3           ), nn.BatchNorm2d(64), nn.ReLU())
-        self.layer5 = nn.Sequential(nn.Conv2d(64,          64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Sequential(
-            nn.Linear(64 * 4 * 4, 128), 
-            nn.ReLU(), 
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_classes))"""
-        
         self.conv1 = nn.Sequential(nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),nn.BatchNorm2d(32))
         self.conv2 = nn.Sequential(nn.Conv2d(32, 64, kernel_size=3, padding=1),nn.BatchNorm2d(64))
         self.conv3 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, padding=1),nn.BatchNorm2d(128))
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(1152, 256)  # Adjusted the input size based on the modified structure
         self.fc2 = nn.Linear(256, num_classes)
-
-    """def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        print(x.size())
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x"""
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
