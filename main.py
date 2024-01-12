8ff1616480de21c9df8b49c4338abd28fa307f58
zceelsc <76179368+zceelsc@users.noreply.github.com>
2024-01-12 21:17:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import subprocess
-
-
 location=r'C:\Users\user\AMLS_assignment23_24-\Datasets'  #change this accordingly
 def run_A_py():
     try:
@@ -8,16 +6,13 @@ def run_A_py():
     except subprocess.CalledProcessError as e:
         print(f"Error running a.py: {e}")
 
-
 def run_B_py():
     try:
         subprocess.run(['python', 'B.py', location], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running B.py: {e}")
 
-
-
-option = input('A or B: ')
+option = input('A or B: ')   #input A or B for binary or multi-class
 # Call the function to run a.py
 if option=='A' or option =='a':
     run_A_py()
