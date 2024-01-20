2d9446b2c8eb5e9763becfa1b77a0d5dc04a525b
zceelsc <76179368+zceelsc@users.noreply.github.com>
2024-01-20 00:47:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import subprocess
-location=r'C:\Users\user\AMLS_assignment23_24-\Datasets'  #change this accordingly
+import os
+
+locationa = r'C:\Users\user\AMLS_assignment23_24-\A'  # Change this accordingly
+locationb = r'C:\Users\user\AMLS_assignment23_24-\B'  # Change this accordingly
+dataset_location = r'C:\Users\user\AMLS_assignment23_24-\Datasets'  # Change this to the desired dataset location
+model_locationa = os.path.join(locationa, 'A_trained_model.pth')
+model_locationb = os.path.join(locationb, 'B_trained_model.pth')
+
 def run_A_py():
+    script_path = os.path.join(locationa, 'A.py')
+    print(f"Attempting to run: {script_path}")
+    
+    # Change the current working directory to the location of A.py
+    os.chdir(locationa)
+
     try:
-        subprocess.run(['python', 'A.py', location], check=True)
+        subprocess.run(['python', script_path, dataset_location, model_locationa], check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error running a.py: {e}")
+        print(f"Error running A.py: {e}")
 
 def run_B_py():
+    script_path = os.path.join(locationb, 'B.py')
+    print(f"Attempting to run: {script_path}")
+    
+    # Change the current working directory to the location of B.py
+    os.chdir(locationb)
+
     try:
-        subprocess.run(['python', 'B.py', location], check=True)
+        subprocess.run(['python', script_path, dataset_location, model_locationb], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running B.py: {e}")
 
-option = input('A or B: ')   #input A or B for binary or multi-class
-# Call the function to run a.py
-if option=='A' or option =='a':
+option = input('A or B: ')  # input A or B for binary or multi-class
+
+# Call the function to run the selected script
+if option.lower() == 'a':
     run_A_py()
-elif option=='B' or option =='b':
+elif option.lower() == 'b':
     run_B_py()
 else:
-    print('input error')
+    print('Input error')
